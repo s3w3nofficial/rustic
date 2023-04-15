8c997893a0f0d4400d9b9d319124a114a0230315
Tomas Pelak <s3w3nofficial@gmail.com>
2023-04-15 12:19:23 +0200

    multiple socket connections on one ip and port

--- examples/proxy/src/main.rs
+++ examples/proxy/src/main.rs
@@ -44,11 +44,15 @@
 use async_std::net::{TcpListener, TcpStream};
 use async_std::prelude::*;
 use async_std::task;
-use libc::{c_int, setsockopt, SOL_SOCKET, SO_REUSEADDR};
+use libc::{c_int, setsockopt, SOL_SOCKET, SO_REUSEADDR, SO_REUSEPORT, sockaddr_in, bind, listen};
+use std::mem;
+use std::os::fd::FromRawFd;
 use std::os::unix::io::{AsRawFd, RawFd};
 
 async fn handle_client(stream: TcpStream) -> std::io::Result<()> {
     // Handle the client connection
+    println!("{}", stream.peer_addr().unwrap());
+
     Ok(())
 }
 
@@ -60,6 +64,10 @@ async fn start_server(port: u16) -> std::io::Result<()> {
         match stream {
             Ok(stream) => {
                 // Spawn a new task to handle the connection
+
+                let socket_fd = listener.as_raw_fd();
+                println!("Socket file descriptor number: {}", socket_fd);
+
                 task::spawn(async move {
                     if let Err(e) = handle_client(stream).await {
                         eprintln!("Error handling connection: {}", e);
@@ -76,8 +84,37 @@ async fn start_server(port: u16) -> std::io::Result<()> {
 }
 
 async fn create_listener(port: u16) -> std::io::Result<TcpListener> {
-    let listener = TcpListener::bind(("127.0.0.1", port)).await.unwrap();
-    set_reuseaddr(&listener)?;
+
+    let sockfd = unsafe { libc::socket(libc::AF_INET, libc::SOCK_STREAM, 0) };
+    if sockfd == -1 {
+        panic!("Error creating socket");
+    }
+
+    let mut serv_addr: sockaddr_in = unsafe { mem::zeroed() };
+    serv_addr.sin_family = libc::AF_INET as u16;
+    serv_addr.sin_port = socket::htons(port);
+    serv_addr.sin_addr.s_addr = socket::htonl(libc::INADDR_ANY as u32);
+
+    let optval: c_int = 1;
+    unsafe {
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval as *const _ as *const _, std::mem::size_of_val(&optval) as u32);
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &optval as *const _ as *const _, std::mem::size_of_val(&optval) as u32);
+    }
+
+    let status = unsafe { bind(sockfd, &serv_addr as *const _ as *const _, mem::size_of_val(&serv_addr) as u32) };
+    if status == -1 {
+        panic!("Error binding socket to address");
+    }
+
+    let status = unsafe { listen(sockfd, 5) };
+    if status == -1 {
+        panic!("Error listening for incoming connections");
+    }
+
+    println!("Startint tcp listener on socket: {}", sockfd);
+
+    let listener = unsafe { TcpListener::from_raw_fd(sockfd) };
+    //set_reuseaddr(&listener)?;
     Ok(listener)
 }
 
@@ -86,6 +123,7 @@ fn set_reuseaddr(listener: &TcpListener) -> std::io::Result<()> {
     let optval: c_int = 1;
     unsafe {
         setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optval as *const _ as *const _, std::mem::size_of_val(&optval) as u32);
+        setsockopt(fd, SOL_SOCKET, SO_REUSEPORT, &optval as *const _ as *const _, std::mem::size_of_val(&optval) as u32);
     }
     Ok(())
 }
